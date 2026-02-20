ad14fafa48bdbdc617b49e3b0f6135ac8704dbb3
jonaykb <jonaykb@gmail.com>
2026-02-20 14:48:07 +0000

    step8-test: added IPropertyReader tests

---
+++ garageSpring/src/test/java/it/dst/garage/utils/IPropertyReaderTest.java
@@ -0,0 +1,17 @@
+package it.dst.garage.utils;
+
+import org.junit.jupiter.api.Test;
+import java.util.Properties;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IPropertyReaderTest {
+
+    @Test
+    void testGetProperties_Success() {
+        Properties props = IPropertyReader.getProperties();
+
+        assertNotNull(props);
+        assertFalse(props.isEmpty());
+        assertEquals("test_url", props.getProperty("db.url"));
+    }
+}
